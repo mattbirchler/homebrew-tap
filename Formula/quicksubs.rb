166dbadd861078631e03b61af8ae5bf5c6666ca1
mattbirchler <mattbirchler@users.noreply.github.com>
2026-07-17 10:14:54 -0500

    quicksubs 1.1.1

--- Formula/quicksubs.rb
+++ Formula/quicksubs.rb
@@ -1,9 +1,9 @@
 class Quicksubs < Formula
   desc "Transcribe audio and video files with Apple's on-device speech engine"
   homepage "https://github.com/mattbirchler/quicksubs"
-  url "https://github.com/mattbirchler/quicksubs/releases/download/v1.1.0/quicksubs-1.1.0.zip"
-  sha256 "76375e2d99d4ea0fc82663a01bac46579514dbefc687e5b5c5a0c19da6de6fc9"
-  version "1.1.0"
+  url "https://github.com/mattbirchler/quicksubs/releases/download/v1.1.1/quicksubs-1.1.1.zip"
+  sha256 "8cb8c4e1339cea7b2166779fa996b09744cbb35c2f6d76da6266edb6310759fc"
+  version "1.1.1"
 
   depends_on macos: :tahoe
 
