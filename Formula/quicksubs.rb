eaa4bdc67415456c1ed1bd9f17a2f101f5f7381b
Matt Birchler <mattbirchler@users.noreply.github.com>
2026-07-28 17:02:33 -0500

    quicksubs 1.5.2

--- Formula/quicksubs.rb
+++ Formula/quicksubs.rb
@@ -1,9 +1,9 @@
 class Quicksubs < Formula
   desc "Transcribe audio and video files with Apple SpeechAnalyzer, OpenAI Whisper, or NVIDIA Parakeet"
   homepage "https://github.com/mattbirchler/quicksubs"
-  url "https://github.com/mattbirchler/quicksubs/releases/download/v1.5.1/quicksubs-1.5.1.zip"
-  sha256 "75564abf72cc04396d790bf1c6213a62894e262408a2d90a57c4d8b39a9b7dd4"
-  version "1.5.1"
+  url "https://github.com/mattbirchler/quicksubs/releases/download/v1.5.2/quicksubs-1.5.2.zip"
+  sha256 "221622fc265055008b3c8753f966c31f54b9609eca06164f8c6ce76705618425"
+  version "1.5.2"
 
   depends_on macos: :tahoe
 
