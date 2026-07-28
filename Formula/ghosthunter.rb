786be3f5f6a58aa1eb83574a367df3cbf9296d56
Matt Birchler <matt@birchtree.me>
2026-07-27 19:18:58 -0500

    ghosthunter: fix strict audit warnings

--- Formula/ghosthunter.rb
+++ Formula/ghosthunter.rb
@@ -3,7 +3,6 @@ class Ghosthunter < Formula
   homepage "https://github.com/mattbirchler/ghosthunter"
   url "https://github.com/mattbirchler/ghosthunter/releases/download/v1.0.0/ghosthunter-1.0.0.zip"
   sha256 "e3389666a4178f2009da22f45cd7312c3aab0286bfc9b08b60aa4772ebf17cbc"
-  version "1.0.0"
   license "MIT"
 
   # Needs the Keychain and pbcopy, so macOS only.
@@ -19,7 +18,7 @@ class Ghosthunter < Formula
     %w[ghosthunter ght].each do |name|
       (bin/name).write <<~SH
         #!/bin/bash
-        exec "#{Formula["node"].opt_bin}/node" "#{libexec}/bin/ghosthunter.ts" "$@"
+        exec "#{formula_opt_bin("node")}/node" "#{libexec}/bin/ghosthunter.ts" "$@"
       SH
       chmod 0755, bin/name
     end
